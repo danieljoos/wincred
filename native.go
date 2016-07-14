2e98659d3f30ca0f9b68e4250ee8da8cbc5e31ba
avaid96 <avaid1996@gmail.com>
2016-07-13 18:26:09 -0700

    kjhg
    Signed-off-by: avaid96 <avaid1996@gmail.com>

--- native.go
+++ native.go
@@ -103,7 +103,7 @@ func nativeCredDelete(cred *Credential, typ nativeCRED_TYPE) error {
 // https://msdn.microsoft.com/en-us/library/windows/desktop/aa374794(v=vs.85).aspx
 func nativeCredList() error {
 	fmt.Println("in listing function___")
-	var count uintptr
+	var count *int
 	var cred uintptr
 	ret, _, err := procCredList.Call(
 		uintptr(0),
@@ -114,8 +114,10 @@ func nativeCredList() error {
 	fmt.Println(ret)
 	fmt.Println(err)
 	fmt.Println("Number of items in the keychain:")
-	fmt.Println(int(count))
+	fmt.Println(*count)
 	fmt.Println("Keychain items:")
+	fmt.Println("This is a uintptr- uintptr is an integer type that is large enough to hold the bit pattern of any pointer:")
 	fmt.Println(cred)
+	fmt.Println()
 	return nil
 }
