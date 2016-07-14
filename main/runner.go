94bbf3045dead9f162307752cd655400b345356b
avaid96 <avaid1996@gmail.com>
2016-07-14 14:47:52 -0700

    hjuo
    Signed-off-by: avaid96 <avaid1996@gmail.com>

--- main/runner.go
+++ main/runner.go
@@ -7,15 +7,8 @@ import (
 
 func main() {
 	fmt.Println("hello")
-	g, err := winc.GetGenericCredential("credzzzz")
-	fmt.Println(err)
-	if g == nil {
-		fmt.Println("error")
-	}
-	fmt.Println(g.UserName)
-	fmt.Println(string(g.CredentialBlob))
 	fmt.Println("list stuff ------------")
-	err = winc.List()
+	err := winc.List()
 	fmt.Println(err)
 }
 
