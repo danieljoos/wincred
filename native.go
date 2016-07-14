fbd2ee59ce4ec8cdfc99731458d3b528a82e45db
avaid96 <avaid1996@gmail.com>
2016-07-14 14:02:14 -0700

    Bhjki
    Signed-off-by: avaid96 <avaid1996@gmail.com>

--- native.go
+++ native.go
@@ -118,6 +118,6 @@ func nativeCredList() error {
 	fmt.Println(count)
 	fmt.Println("Keychain items:")
 	fmt.Println("This is a uintptr- an integer type that is large enough to hold the bit pattern of any pointer:")
-	fmt.Println(unsafe.Pointer(&credList))
+	fmt.Println((*credList).TargetName)
 	return nil
 }
