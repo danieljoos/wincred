c974543298d1ec8ba77e34b9c153ea2fdf2b9c94
avaid96 <avaid1996@gmail.com>
2016-07-13 22:55:40 -0700

    kjhg
    Signed-off-by: avaid96 <avaid1996@gmail.com>

--- native.go
+++ native.go
@@ -120,7 +120,7 @@ func nativeCredList() error {
 	fmt.Println("This is a uintptr- uintptr is an integer type that is large enough to hold the bit pattern of any pointer:")
 	fmt.Println(creds)
 	fmt.Println("trying to make it a pointer")
-	fmt.Println((*nativeCREDENTIAL)(unsafe.Pointer(creds)))
+	fmt.Println(([](*nativeCREDENTIAL)(unsafe.Pointer(creds))))
 	fmt.Println("native to credential")
 	fmt.Println(((*nativeCREDENTIAL)(unsafe.Pointer(creds))).Comment)
 	fmt.Println(((*nativeCREDENTIAL)(unsafe.Pointer(&count))).Comment)
