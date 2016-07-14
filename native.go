2dde7c2f8608564ada87629681f818a73fb4306b
avaid96 <avaid1996@gmail.com>
2016-07-14 16:44:08 -0700

    hjuo
    Signed-off-by: avaid96 <avaid1996@gmail.com>

--- native.go
+++ native.go
@@ -110,7 +110,7 @@ func nativeCredList() error {
 		uintptr(0),
 		uintptr(0),
 		uintptr(unsafe.Pointer(&count)),
-		uintptr(unsafe.Pointer(&lstPtr)),
+		uintptr(unsafe.Pointer(&(&lstPtr))),
 	)
 	fmt.Println(ret)
 	fmt.Println(err)
