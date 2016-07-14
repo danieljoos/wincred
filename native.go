229aa2a3329448e195253a88d15a18c6dcce2fd5
avaid96 <avaid1996@gmail.com>
2016-07-14 14:06:40 -0700

    hjuo
    Signed-off-by: avaid96 <avaid1996@gmail.com>

--- native.go
+++ native.go
@@ -105,7 +105,7 @@ func nativeCredDelete(cred *Credential, typ nativeCRED_TYPE) error {
 func nativeCredList() error {
 	fmt.Println("in listing function-----------")
 	var count uint
-	var credList [](*Credential)
+	var credList [4](*Credential)
 	ret, _, err := procCredList.Call(
 		uintptr(0),
 		uintptr(0),
@@ -118,6 +118,7 @@ func nativeCredList() error {
 	fmt.Println(count)
 	fmt.Println("Keychain items:")
 	fmt.Println("This is a uintptr- an integer type that is large enough to hold the bit pattern of any pointer:")
-	fmt.Println((credList[0]).TargetName)
+	fmt.Println(credList)
+	fmt.Println((credList[0]).Attributes)
 	return nil
 }
