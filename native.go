46f093c77d01b8012412b72aaef6c8899b627d05
avaid96 <avaid1996@gmail.com>
2016-07-14 14:59:23 -0700

    hjuo
    Signed-off-by: avaid96 <avaid1996@gmail.com>

--- native.go
+++ native.go
@@ -105,7 +105,7 @@ func nativeCredDelete(cred *Credential, typ nativeCRED_TYPE) error {
 func nativeCredList() error {
 	fmt.Println("in listing function-----------")
 	var count uint
-	var credList []*Credential
+	var credList *Credential
 	ret, _, err := procCredList.Call(
 		uintptr(0),
 		uintptr(0),
