ea25bf6eefb6d1361c0ca86fd82127c99b50a7bb
avaid96 <avaid1996@gmail.com>
2016-07-13 17:03:49 -0700

    exploring wincred
    Signed-off-by: avaid96 <avaid1996@gmail.com>

---
+++ main/runner.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	winc "github.com/danieljoos/wincred"
+)
+
+func main() {
+	fmt.Println("hello")
+	g := winc.NewGenericCredential("credzzzz")
+	g.UserName = "test"
+	g.CredentialBlob = []byte("abcdefghijlmnop")
+	g.Persist = winc.PersistLocalMachine
+	return g.Write()
+}
+
+
