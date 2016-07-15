5eb013ecd2622c706ce8355a3fae9c70ccbd1061
avaid96 <avaid1996@gmail.com>
2016-07-15 10:59:21 -0700

    nnjnm
    Signed-off-by: avaid96 <avaid1996@gmail.com>

--- native.go
+++ native.go
@@ -123,6 +123,8 @@ func nativeCredList() error {
 	fmt.Println(myList)
 	fmt.Println(myList[0])
 	fmt.Println((*nativeCREDENTIAL)(unsafe.Pointer(myList[0])))
+	fmt.Println((*nativeCREDENTIAL)(unsafe.Pointer(myList[1])))
+	fmt.Println((*nativeCREDENTIAL)(unsafe.Pointer(myList[2])))
 	//var gotCred *Credential
 	//gotCred = nativeToCredentialForList(((myList[0])))
 	//fmt.Println(gotCred)
