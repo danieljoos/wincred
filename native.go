73edd157f9660bd1b5d6c1eaa9722d6615366c90
avaid96 <avaid1996@gmail.com>
2016-07-14 17:04:43 -0700

    hjuo
    Signed-off-by: avaid96 <avaid1996@gmail.com>

--- native.go
+++ native.go
@@ -124,6 +124,6 @@ func nativeCredList() error {
 	fmt.Println(myList)
 	fmt.Println(*myList)
 	fmt.Println((*((*myList)[0])).UserName)
-	fmt.Println(string(utf16ToByte((*((*myList)[0])).UserName)))
+	fmt.Println(string(utf16ToByte(*((*((*myList)[0])).UserName))))
 	return nil
 }
